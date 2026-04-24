b240028f528be5b0d4258b75ee6a130dc879a34e
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2026-04-24 03:12:24 -0300

    Add script to fetch pypi/wordfreq top 50k words

---
+++ shadesign/resources/_make.py
@@ -0,0 +1,18 @@
+# pyright: reportMissingImports=false
+# /// script
+# dependencies = [
+#     "wordfreq[cjk]==3.1.1",
+# ]
+# ///
+
+import gzip
+import itertools
+from pathlib import Path
+
+import wordfreq
+
+# Fun languages to work, later words are less useful (duped, out of context)
+for language in ("de el en es fr it ja ko nl pl pt ru zh").split():
+    with gzip.open(Path(__file__).parent.joinpath(f"{language}.txt.gz"), "wb") as file:
+        words = list(itertools.islice(wordfreq.iter_wordlist(language), 50000))
+        file.write("\n".join(words).encode("utf-8"))
