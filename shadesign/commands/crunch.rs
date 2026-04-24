7e0c43d150bc91e31b995bcd2f111ae8f71db98a
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2026-04-24 06:58:14 -0300

    Add filtering by word, symmetric, even

--- shadesign/commands/crunch.rs
+++ shadesign/commands/crunch.rs
@@ -4,20 +4,32 @@ use crate::*;
 pub struct CrunchCommand {
 
     #[command(subcommand)]
-    words: WordFactory,
+    input: WordFactory,
 
-    /// How many words to score
+    /// How many words to score (warn: O(N^2) complexity!)
     #[arg(short='t', long, default_value_t=50000)]
     total: usize,
 
-    /// Minimum overlap to store results
-    #[arg(short='m', long, default_value_t=3)]
-    min: usize,
+    /// (Filter) Minimum overlap to store results
+    #[arg(short='o', long, default_value_t=3)]
+    overlap: usize,
+
+    /// (Filter) Comparison must contain these words
+    #[arg(short, long)]
+    word: Option<Vec<String>>,
+
+    /// (Filter) Only keep even length words
+    #[arg(long)]
+    even: bool,
+
+    /// (Filter) Only keep half-word overlaps
+    #[arg(long)]
+    symmetric: bool,
 }
 
 impl CrunchCommand {
     pub fn run(&self) {
-        let words: Vec<String> = self.words.get()
+        let words: Vec<String> = self.input.get()
             .into_iter().take(self.total)
             .filter(|w| w.chars().all(|c| c.is_alphabetic()))
             .map(|s| s.to_lowercase())
@@ -35,12 +47,20 @@ impl CrunchCommand {
                 let mut scores: Vec<Score> = Vec::new();
 
                 for B in words.clone() {
+
+                    // Skip non matching words early
+                    if let Some(filter) = &self.word {
+                        if !(filter.contains(&A) || filter.contains(&B)) {
+                            continue;
+                        }
+                    }
+
                     let score = Score::compute(A.clone(), B.clone());
 
-                    // Skip boring overlaps
-                    if score.overlap < self.min {
-                        continue;
-                    }
+                    // Apply result rejection filters
+                    if score.overlap < self.overlap {continue;}
+                    if self.symmetric && !score.symmetric {continue;}
+                    if self.even && !score.even {continue;}
 
                     scores.push(score);
                 }
